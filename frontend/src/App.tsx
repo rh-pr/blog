6c2e61bb39848fca7bc23cb0682128523c1fefd8
Ruslana <ruslana.hural.j@gmail.com>
2025-01-12 22:16:30 +0100

    add base routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Layout from './pages/Layout'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout />} />
+        <Route path='/' element={<Layout />} >
+          <Route path='/'  element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from '../components/layout/Header'
+import Footer from '../components/layout/Footer'
 
 function Layout() {
   return (
     <div>
-        Layout
+       <Header />
+       <div className='flex'>
+            <Outlet />
+       </div>
+    <Footer />
     </div>
   )
 }
