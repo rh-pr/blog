ddff9252e03d100d168198332ac20abe5bd770a8
Ruslana <ruslana.hural.j@gmail.com>
2025-01-12 23:02:21 +0100

    create footer

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -2,7 +2,13 @@
 
 function Footer() {
   return (
-   <div>Hi</div>
+   <div className="p-5 fontn-roboto flex  w-screen gap-2 justify-between items-start ">
+      <div className="w-6/12 flex flex-col items-start">
+        <p className="font-black text-xl">Blog</p>
+        <p className="font-medium text-sm">Pets application</p>
+      </div>
+      <p className="font-black self-end">RH-Developer</p>
+   </div>
   )
 }
 
